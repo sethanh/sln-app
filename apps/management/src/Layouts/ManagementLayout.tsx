44c4343bc1d4d60ce14c3376517ffc840636421e
seeasysalon <sent@easysalon.vn>
2024-09-13 15:33:57 +0700

    feat sidebar component

--- apps/management/src/Layouts/ManagementLayout.tsx
+++ apps/management/src/Layouts/ManagementLayout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {MainLayout} from '@my-monorepo/ui'
+import { MainLayout } from '@my-monorepo/ui'
+import { ManagementSidebar } from './ManagementSidebar'
 
 export const ManagementLayout: React.FC = () => {
   return (
     <MainLayout
       header={<div>Project Management Dashboard</div>}
-      sidebar={<div>Sidebar Links</div>}
+      sidebar={<ManagementSidebar/>}
       footer={<footer>Footer Content</footer>}
       content={<div>Main Content Goes Here</div>}
     />
