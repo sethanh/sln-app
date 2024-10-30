7dcd60a5f07b630d0dcd663aba0f74ca79456f15
ThuyVan032004 <thuyvan18032004@gmail.com>
2024-10-30 14:18:51 +0700

    commit to checkout master

--- apps/management/src/Pages/Financials/FinancialPage.tsx
+++ apps/management/src/Pages/Financials/FinancialPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './FinancialPage.css'
 
 export const FinancialPage: React.FC = () => {
-
     return (
         <div className={`management-content-body`}>
             
