2ef96b9e4ec5edbfba9cae4211f244e882784d06
seeasysalon <sent@easysalon.vn>
2024-10-31 11:46:37 +0700

    feat(_): call api

--- apps/management/src/Pages/Documents/Document/DocumentPage.tsx
+++ apps/management/src/Pages/Documents/Document/DocumentPage.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {apiFetch} from '@my-monorepo/utils'
 
 const DocumentPage: React.FC = () => {
+    useEffect(() => {
+      }, []);
+
     return (
         <div>
             DocumentPage
