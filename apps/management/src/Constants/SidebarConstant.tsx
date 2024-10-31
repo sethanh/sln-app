a917c2465ca1701f64cb41102712084385adb610
ThuyVan032004 <thuyvan18032004@gmail.com>
2024-10-31 08:27:54 +0700

    Fix and check conflicts

--- apps/management/src/Constants/SidebarConstant.tsx
+++ apps/management/src/Constants/SidebarConstant.tsx
@@ -1,32 +1,30 @@
 import { AimOutlined, ClusterOutlined, RiseOutlined, UserOutlined, ScheduleOutlined, SlidersOutlined, UnorderedListOutlined } from "@ant-design/icons";
-import { FinancialPage, TaskPage } from "../Pages";
 import { ISidebarItemProps } from "@my-monorepo/ui";
-// import { FinancialPersonal } from "../Pages/Financials/Personal/FinancialPersonal";
 
 const taskSidebarConstants : ISidebarItemProps[]= [
     {
         label: "Today",
         Icon: <ScheduleOutlined />,
         path: 'task/today',
-        page: <TaskPage />
+        page: 
     },
     {
         label: "All",
         Icon: <UnorderedListOutlined />,
         path: 'task/all',
-        page: <TaskPage />
+        page: 
     },
     {
         label: "Management",
         Icon: <ClusterOutlined />,
         path: 'task/management',
-        page: <TaskPage />
+        page: 
     },
     {
         label: "Back Log",
         Icon: <SlidersOutlined />,
         path: 'task/back-log',
-        page: <TaskPage />
+        page: 
     }
 ] 
 
@@ -35,25 +33,25 @@ const financialSidebarConstants : ISidebarItemProps[]= [
         label: "Personal",
         Icon: <UserOutlined />,
         path: 'financial/personal',
-        page : <FinancialPage />
+        page : 
     },
     {
         label: "Planning",
         Icon: <RiseOutlined />,
         path: 'financial/planing',
-        page : <FinancialPage />
+        page : 
     },
     {
         label: "Objective",
         Icon: <AimOutlined />,
         path: 'financial/objective',
-        page : <FinancialPage />
+        page : 
     },
     {
         label: "Management",
         Icon: <ClusterOutlined />,
         path: 'financial/management',
-        page : <FinancialPage />
+        page : 
     }
 ]
 
