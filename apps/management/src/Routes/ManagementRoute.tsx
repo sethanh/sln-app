2f908a5d5599fd6a826a4971ac327946fb9dfc6b
ThuyVan032004 <thuyvan18032004@gmail.com>
2024-10-31 08:34:17 +0700

    Fix conflicts

--- apps/management/src/Routes/ManagementRoute.tsx
+++ apps/management/src/Routes/ManagementRoute.tsx
@@ -1,11 +1,9 @@
 import { FC } from 'react';
-import {RouteApp} from '@my-monorepo/ui'
-import { financialSidebarConstants, taskSidebarConstants } from '@my-monorepo/management';
 
 export const ManagementRoute: FC= () => {
     return (
         <RouteApp
-            routes={[...financialSidebarConstants, ...taskSidebarConstants]}
+            
         />
     );
 };
