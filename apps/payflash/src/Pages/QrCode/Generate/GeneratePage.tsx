69a61e4a44870caeadc5bab19c3245f7988a4258
seeasysalon <sent@easysalon.vn>
2024-11-19 22:19:56 +0700

    feat generation code

--- apps/payflash/src/Pages/QrCode/Generate/GeneratePage.tsx
+++ apps/payflash/src/Pages/QrCode/Generate/GeneratePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {QRCodeGenerator} from './Components'
 
 const GeneratePage: React.FC = () => {
     return (
         <div>
             GeneratePage
+            <QRCodeGenerator/>
         </div>
     );
 };
--- apps/payflash/src/Pages/QrCode/Generate/index.tsx
+++ apps/payflash/src/Pages/QrCode/Generate/index.tsx
@@ -1,1 +1,2 @@
 export * from './GeneratePage'
+export * from './Components'
