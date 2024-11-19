e5018a75147f6f1456199e5b8497e764364dded3
seeasysalon <sent@easysalon.vn>
2024-11-19 20:38:29 +0700

    feat language

--- apps/management/src/Root/Sagas/Account/account.action.ts
+++ apps/management/src/Root/Sagas/Account/account.action.ts
@@ -1,25 +1,13 @@
-import { put, takeLatest,select } from 'redux-saga/effects'
+import { put } from 'redux-saga/effects'
 import { setAccount, clearAccount} from '@my-monorepo/management/Root'
 import { PayloadAction } from '@reduxjs/toolkit'
 
-function* onSetAccount({payload}:PayloadAction<string | null>){
+export function* onSetAccount({payload}:PayloadAction<string | null>){
   console.log(payload);
   yield put(setAccount(payload))
 }
 
 
-function* onClearAccount({}:PayloadAction<any>){
-  console.log('clear');
+export function* onClearAccount({}:PayloadAction<any>){
   yield put(clearAccount())
 }
-
-export function* watchClearAccount() {
-  console.log('wclear');
-  yield takeLatest( setAccount.type, onClearAccount);
-}
-
-export function* watchSetAccount() {
-  console.log('wset');
-  yield takeLatest( setAccount.type, onSetAccount)
-}
-
--- apps/management/src/Root/Sagas/Account/account.saga.ts
+++ apps/management/src/Root/Sagas/Account/account.saga.ts
@@ -1,5 +1,15 @@
-import { all } from 'redux-saga/effects'
-import { watchSetAccount, watchClearAccount } from './account.action'
+import { all, takeLatest } from 'redux-saga/effects'
+import { setAccount } from '@my-monorepo/management/Root';
+import {onClearAccount, onSetAccount} from './account.action'
+
+function* watchClearAccount() {
+    yield takeLatest( setAccount.type, onClearAccount);
+  }
+  
+function* watchSetAccount() {
+    yield takeLatest( setAccount.type, onSetAccount)
+}
+
 export function* accountSaga() {
     yield all([
        watchSetAccount,
