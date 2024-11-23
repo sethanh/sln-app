c734834b2c9792e1facf9b45770fc618a6ee1927
seeasysalon <sent@easysalon.vn>
2024-11-23 16:19:28 +0700

    feat env

--- apps/payflash/src/Constants/UrlConstant.ts
+++ apps/payflash/src/Constants/UrlConstant.ts
@@ -1,4 +1,4 @@
-const apiHost = "http://localhost:5153"
+const apiUrl = import.meta.env.VITE_API_URL;
 
-export const accountUrl = apiHost + "/Accounts" 
+export const accountUrl = apiUrl + "/Accounts" 
 export const googleLoginAccountUrl = accountUrl + "/google-login"
