cbf30ebe363bb67c3c9d59534a65799410696031
seeasysalon <sent@easysalon.vn>
2024-11-23 10:58:24 +0700

    fetch call api

---
+++ apps/payflash/src/Constants/UrlConstant.ts
@@ -0,0 +1,4 @@
+const apiHost = "http://localhost:5153"
+
+export const accountUrl = apiHost + "/Accounts" 
+export const googleLoginAccountUrl = accountUrl + "/google-login"
--- apps/payflash/src/Pages/QrCode/QrHistory/Components/GoogleLogin.tsx
+++ apps/payflash/src/Pages/QrCode/QrHistory/Components/GoogleLogin.tsx
@@ -1,33 +1,56 @@
-import {GoogleAuthLogin, IGoogleTokenResponse} from '@my-monorepo/ui'
+import { GoogleOutlined } from '@ant-design/icons';
+import { GoogleAuthLogin, IGoogleTokenResponse } from '@my-monorepo/ui'
+import { handleApiFetch } from '@my-monorepo/utils'
+import { IRequestOptions } from 'packages/utils/src/services/IRequestOptions';
+import { googleLoginAccountUrl } from '@my-monorepo/payflash/Constants'
+import { TokenModel } from '@my-monorepo/payflash/Models'; 
 
 export const GoogleLogin: React.FC = () => {
-    return (
-      <div>
-        <h1>Custom Google Login Example</h1>
-        <GoogleAuthLogin onHandleSuccess={(token: IGoogleTokenResponse)=>{console.log(token)}} onHandleError={()=> {console.log('error')}}>
-          {(login) => (
-            <button
-              onClick={login}
-              style={{
-                padding: '10px 20px',
-                backgroundColor: '#4285F4',
-                color: 'white',
-                fontSize: '16px',
-                fontWeight: 'bold',
-                border: 'none',
-                borderRadius: '5px',
-                cursor: 'pointer',
-              }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
-                alt="Google Logo"
-                style={{ width: '20px', height: '20px', marginRight: '10px', verticalAlign: 'middle' }}
-              />
-              Sign in with Google
-            </button>
-          )}
-        </GoogleAuthLogin>
-      </div>
+  const onHandleSuccess = async (token: IGoogleTokenResponse) => {
+
+    var option: IRequestOptions = {
+      method: 'POST',
+      body: {
+        accessToken : token.access_token
+      }
+    }
+
+    const login = await handleApiFetch<TokenModel>(
+      googleLoginAccountUrl, 
+      option, 
+      () => { }, 
+      () => { }
     );
-  };
+
+    console.log('login', login);
+  }
+
+
+  return (
+    <div>
+      <h1>Custom Google Login Example</h1>
+      <GoogleAuthLogin onHandleSuccess={onHandleSuccess} onHandleError={() => { console.log('error') }}>
+        {(login) => (
+          <div
+            onClick={login}
+            style={{
+              padding: '10px 20px',
+              backgroundColor: '#4285F4',
+              color: 'white',
+              fontSize: '16px',
+              fontWeight: 'bold',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              display: 'flex',
+              width: 'fit-content'
+            }}
+          >
+            <GoogleOutlined />
+            <div style={{ marginLeft: 8 }}> Sign in with Google </div>
+          </div>
+        )}
+      </GoogleAuthLogin>
+    </div>
+  );
+};
