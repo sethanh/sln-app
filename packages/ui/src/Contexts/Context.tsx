f2559778eead6ffe44549ecf70498a59bfd95050
ThuyVan032004 <thuyvan18032004@gmail.com>
2024-12-08 22:14:06 +0700

    Fix conflict

--- packages/ui/src/Contexts/Context.tsx
+++ packages/ui/src/Contexts/Context.tsx
@@ -1,6 +1,11 @@
 import React, { useContext, useState } from "react";
 import { IContextInit, IContextProvider } from "./IContext";
 
+export function contextInit <T>(initial_value: T) {
+    const context = React.createContext(initial_value);
+    return context
+}
+
 export const ContextProvider = <T,>({ children, Context } : IContextProvider<T>) => {
     const {value: initialValue, setValue: setInitialValue} = useContext<IContextInit<T>>(Context);
     const [value, setValue] = useState(initialValue);
