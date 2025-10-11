9dc056ed55fd20d6e296ea384261f74c6570c293
seeasysalon <sent@easysalon.vn>
2025-10-11 08:22:13 +0700

    Feat(_): card acctive

--- apps/contact/src/Pages/Home/HomePage.tsx
+++ apps/contact/src/Pages/Home/HomePage.tsx
@@ -1,34 +1,43 @@
 import { urlConstant } from "@my-monorepo/contact/Constants";
 import { ContactResponse } from "@my-monorepo/contact/Models";
 import { useContactHttpQuery } from "@my-monorepo/contact/Root";
-import { Block, FlexBox } from "@my-monorepo/ui";
+import { FlexBox } from "@my-monorepo/ui";
 import { DigitalBusinessCard } from "./Components/DigitalBusinessCard";
-import { Spin } from "antd";
+import { Row, Spin } from "antd";
+import './HomePage.css';
 
 
 interface HomeProps {
-    domain: string;
+  domain: string;
 }
 
 export function HomePage(props: HomeProps) {
-    const slugToQuery = props.domain ?? "";
+  const slugToQuery = props.domain ?? "";
 
-    const { data: contact, isLoading } = useContactHttpQuery<ContactResponse>({
-        url: urlConstant.contact.contactByProfileName,
-        method: "GET",
-        queryParams: {
-            profileName: slugToQuery
-        }
+  const { data: contact, isLoading } = useContactHttpQuery<ContactResponse>({
+    url: urlConstant.contact.contactByProfileName,
+    method: "GET",
+    queryParams: {
+      profileName: slugToQuery
     }
-    );
+  }
+  );
 
-    return (
-      <Block width={'100vw'} height={'100vh'}>
-        <Spin spinning={isLoading}>
-          <FlexBox flex={1} width={'100%'} height={'100%'} alignItems="center" justifyContent="center">
-            <DigitalBusinessCard contact={contact}/>
+  return (
+    <Spin spinning={isLoading}>
+      <Row justify="center" align="middle" style={{ minHeight: '100vh', padding: '16px', position: 'relative', background:'linear-gradient(145deg, #f6f5fdff, #e5e5f3ff)' }}>
+        <div className="bgh-1"/>
+        <div className="bgh-2"/>
+        <div className="bgh-3"/>
+        <div className="bgh-4"/>
+        <div className="bgh-5"/>
+        <div className="bgh-6"/>
+        <FlexBox flex={1} width={'100%'} height={'100%'} alignItems="center" justifyContent="center" direction="column">
+          <FlexBox flex={1} direction="column">
+            <DigitalBusinessCard contact={contact} />
           </FlexBox>
-        </Spin>
-      </Block>
-    );
+        </FlexBox>
+      </Row>
+    </Spin>
+  );
 };
