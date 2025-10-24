ca2a4305308e1fb5c8d97ad7c0c23e4086a53257
seeasysalon <sent@easysalon.vn>
2025-10-24 09:09:34 +0700

    Feat(_): generate

--- apps/payflash/src/Pages/QrCode/Generate/GeneratePage.tsx
+++ apps/payflash/src/Pages/QrCode/Generate/GeneratePage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {QRCodeGenerator} from './Components'
+import { FlexBox, TextCommon } from '@my-monorepo/ui';
 
 const GeneratePage: React.FC = () => {
     return (
-        <div>
-            GeneratePage
+        <FlexBox direction='column' gap={24}>
+            <TextCommon fontWeight={600} fontSize={24}>GeneratePage</TextCommon> 
             <QRCodeGenerator/>
-        </div>
+        </FlexBox>
     );
 };
 
